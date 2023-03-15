613010218c02d51b2a30947e5ae913c54e79f805
joshua8321 <j.milo.mack@gmail.com>
2023-03-15 11:23:28 -0400

    fix: add isupervault interface

---
+++ contracts/interfaces/isupervault_v2.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+interface ISupervaultV2 {
+    function estimatedPoolSize() external view returns (uint256);
+
+    function deposit(uint256 amount) external;
+
+    function withdraw(uint256 shares) external;
+
+    function capitalToken() external view returns (address);
+
+    function balanceOf() external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+}
